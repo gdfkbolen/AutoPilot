d2cdbf7c4c5d32cef75a4aa29f9668b76371e2e7
gdfkbolen <167641633+gdfkbolen@users.noreply.github.com>
2024-04-20 12:42:12 -0500

    Add files via upload

---
+++ AutopilotEnroll.ps1
@@ -0,0 +1,39 @@
+#Set GroupTag Variable this is the orderID in Entra, can be used to create Dynamic Groups and is tied to the device through AutoPilot resets
+$StoreLocation = ""
+while ([string]::IsNullOrWhiteSpace($StoreLocation)) {
+    $StoreLocation = Read-Host -Prompt "Enter Store Location no spaces - examples: SpringfieldWest, Eagleville"
+    if ([string]::IsNullOrWhiteSpace($StoreLocation)) {
+        Write-Host "Store location is mandatory and not be empty. Please provide a value."
+    }
+}
+$Brand = ""
+while ([string]::IsNullOrWhiteSpace($Brand)) {
+    $Brand = Read-Host -Prompt "Enter Brand, GDF or CODES"
+    if ([string]::IsNullOrWhiteSpace($Brand)) {
+        Write-Host "Brand is mandatory and not be empty. Please provide a value."
+    }
+}
+$DeviceType = ""
+while ([string]::IsNullOrWhiteSpace($DeviceType)) {
+    $DeviceType = Read-Host -Prompt "Enter DeviceType or Use, example: Kiosk, POS, LT"
+    if ([string]::IsNullOrWhiteSpace($DeviceType)) {
+        Write-Host "DeviceType is mandatory and not be empty. Please provide a value."
+    }
+}
+
+$grouptag = $Brand+$StoreLocation+$DeviceType
+
+
+Read-Host 
+$devicename = Read-Host -Prompt "Enter the desired Computer Name - Optional, Default Value is BrandInitial-First5location-DeviceType"
+
+# Check if the computername parameter is provided
+if (-not [string]::IsNullOrWhiteSpace($devicename)) {
+    $computername = $devicename
+} else {
+    $computername= "Default value"  # Set a default value if the optional parameter is not provided
+}
+
+Set-ExecutionPolicy Unrestricted 
+Install Get-WindowsAutoPilotInfo --Confirm:$false
+Get-WindowsAutoPilotInfo -Online -Groupgrouptag $grouptag -AssignedComputerName $computername
